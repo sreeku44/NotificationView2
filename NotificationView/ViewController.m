07280ca891f43a4deec56276f823c83f5a60e4a6
sreeku44 <2sreeku44@gmail.com>
2017-02-10 18:18:42 -0600

    2nd Commit

--- NotificationView/ViewController.m
+++ NotificationView/ViewController.m
@@ -16,7 +16,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.notificationView  = [[UIView alloc]init];
+    
+    self.notificationView.frame = CGRectMake(0, 0, 414.2, 735.8);
+    
+    self.notificationView.backgroundColor = [UIColor colorWithRed:190.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0];
+    
+    [self.view addSubview:self.notificationView];
+    
+    
+    
+    UILabel *notificationLabel = [[UILabel alloc]init];
+    
+    notificationLabel.frame = CGRectMake(135, 650, 150, 50);
+    
+    notificationLabel.text = @" Notification View ";
+    
+    [self.notificationView addSubview:notificationLabel];
+    
+    
+    UISwipeGestureRecognizer *swipeUpGestureRecognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeUP: ) ];
+    
+    [self.notificationView addGestureRecognizer:swipeUpGestureRecognizer];
+    
+    swipeUpGestureRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+    
+    UISwipeGestureRecognizer *swipeDownGestureRecognizer =[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeDown:)];
+    
+    swipeDownGestureRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
+    
+    [self.view addGestureRecognizer:swipeDownGestureRecognizer];
+   // [self.notificationView addGestureRecognizer:swipeDownGestureRecognizer];
+    
+}
+
+-(void) swipeUP: (UISwipeGestureRecognizer*) notificationUP {
+    
+ self.notificationView =  notificationUP.view;
+    
+    [UIView animateWithDuration:5.0 animations:^{
+        self.notificationView.frame = CGRectMake (0,-738.8,414.2,738.8); }];
+
+}
+
+-(void) swipeDown: (UISwipeGestureRecognizer*) notificationDown{
+    
+   // self.notificationView = notificationDown.view;
+    
+    self.view = notificationDown.view;
+    
+        [UIView animateWithDuration:5.0 animations:^{
+            self.notificationView.frame = CGRectMake (0,0,414.2,738.8); }];
+
 }
 
 
